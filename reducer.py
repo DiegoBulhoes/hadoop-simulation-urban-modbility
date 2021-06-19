a0ffc21a162370835cf4fdece1e5f3f878553e60
DiegoBulhoes <diegobulhoesm@gmail.com>
2021-06-18 22:59:22 -0400

    Add mapper and reducer - sum of gases - SUMO

--- reducer.py
+++ reducer.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
 import sys
+from operator import add
 
-currently_word = None
-currently_count = 0
+currently_vehicle = None
+currently_data = list(0 for i in range(13))
 
 for line in sys.stdin:
-    word, count = line.split('\t')
-    count = int(count)
-    if word == currently_word:
-        currently_count += count
+    input = line.replace('\n', '').split(';')
+    data = [float(i) for i in input[1:11]]
+    vehicle_name = input[-1]
+    if vehicle_name == currently_vehicle:
+        currently_data = list(map(add, currently_data, data))
     else:
-        if currently_word:
-            print('{0};{1}'.format(currently_word, currently_count))
-        currently_word = word
-        currently_count = count
+        if currently_vehicle:
+            print('{0};{1}'.format(currently_vehicle, currently_data))
+        currently_vehicle = vehicle_name
+        currently_data = data
 
-if currently_word == word:
-    print('{0};{1}'.format(currently_word, currently_count))
+if currently_vehicle == vehicle_name:
+    print('{0};{1}'.format(currently_vehicle, currently_data))
