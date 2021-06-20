1f92a09dc1054fde61e0542753277da9ee0b582c
DiegoBulhoes <diegobulhoesm@gmail.com>
2021-06-20 12:29:17 -0400

    Fix reduction function

--- reducer.py
+++ reducer.py
@@ -5,6 +5,19 @@ from operator import add
 
 currently_vehicle = None
 currently_data = list(0 for i in range(13))
+currently_data_min = list((float('inf')) for i in range(13))
+currently_data_max = list((float('-inf')) for i in range(13))
+
+qtd = 0
+max = (float('-inf'))
+min = (float('inf'))
+
+
+def output_reducer(currently_data, currently_vehicle, mean):
+    str_currently_data = ';'.join(map(str, currently_data))
+    str_mean = ';'.join(map(str, mean))
+    return f'{currently_vehicle};{str_currently_data};{str_mean}'
+
 
 for line in sys.stdin:
     input = line.replace('\n', '').split(';')
@@ -12,11 +25,16 @@ for line in sys.stdin:
     vehicle_name = input[-1]
     if vehicle_name == currently_vehicle:
         currently_data = list(map(add, currently_data, data))
+        currently_data_max = [n if n < m else m for n, m in zip(currently_data_max, data)]
+        currently_data_min = [n if n > m else m for n, m in zip(currently_data_max, data)]
+        qtd += 1
     else:
         if currently_vehicle:
-            print('{0};{1}'.format(currently_vehicle, currently_data))
+            mean = list(map(lambda x: x / qtd, currently_data))
+            print(output_reducer(currently_data, currently_vehicle, mean))
         currently_vehicle = vehicle_name
         currently_data = data
 
 if currently_vehicle == vehicle_name:
-    print('{0};{1}'.format(currently_vehicle, currently_data))
+    mean = list(map(lambda x: x / qtd, currently_data))
+    print(output_reducer(currently_data, currently_vehicle, mean))
